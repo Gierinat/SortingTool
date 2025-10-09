d59cf646fb27207aa7539adff22e2de6e3f2b769
Michal Gierlach <michal.gierlach@gmail.com>
2025-10-09 12:37:34 +0200

    adding support for words and lines

--- main.py
+++ main.py
@@ -5,31 +5,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-dataType", choices=["long", "line", "word"])
 args = parser.parse_args()
 calculation_flag = args.dataType if args.dataType else "word"
+data = []
 
 
-def long_calculations():
+def get_input(input_type):
     while True:
         try:
-            for num in input().split():
-                data.append(int(num))
+            if input_type == "long":
+                for num in input().split():
+                    data.append(int(num))
+            elif input_type == "word":
+                for word in input().split():
+                    data.append(word)
+            else:
+                for line in input().splitlines():
+                    data.append(line)
         except EOFError:
             break
 
 
-# Input gathering
-data = []
 if calculation_flag == "long":
-    long_calculations()
+    get_input("long")
+elif calculation_flag == "word":
+    get_input("word")
 else:
-    print("TBD")
+    get_input("line")
 
+print(data)
 # Calculations
-total_numbers = len(data)
-greatest_number = max(data)
-greatest_number_occurrences = data.count(greatest_number)
+total = len(data)
+greatest = max(data) if calculation_flag == "long" else max(data, key=len)
+greatest_occurrences = data.count(greatest)
 
 # Result presentation
-result_string = f"""Total numbers: {total_numbers}.
-The greatest number: {greatest_number} ({greatest_number_occurrences} time(s))."""
+topic = "numbers" if calculation_flag == "long" else "lines" if calculation_flag == "line" else "words"
+top = "greatest number" if calculation_flag == "long" else "longest line" if calculation_flag == "line"\
+    else "longest word"
+result_string = f"""Total {topic}: {total}.
+The {top}: {greatest} ({greatest_occurrences} time(s))."""
 
 print(result_string)
