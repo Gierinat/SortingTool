434409a09910ba5e5a4f1e407a1af24be23e1b27
Michal Gierlach <michal.gierlach@gmail.com>
2025-10-29 18:16:45 +0100

    sorting by count

--- main.py
+++ main.py
@@ -39,9 +39,6 @@ else:
 
 # Calculations
 total = len(data)
-greatest = max(data) if data_type == "long" else max(data, key=len)
-greatest_occurrences = data.count(greatest)
-percentage_occurrence = int(greatest_occurrences / total * 100)
 
 
 # Result presentation
@@ -49,12 +46,24 @@ def sort_data(dat, sort):
     if sort == "natural" and data_type in ("long", "word"):
         sorted_data = sorted(dat)
         sorted_data = map(str, sorted_data)
-        join_result = " ".join(sorted_data)
-        return join_result
+        result = " ".join(sorted_data)
+        return result
     elif sort == "natural" and data_type == "line":
-        sorted_data = sorted(dat, key=len, reverse=True)
-        join_result = "\n".join(sorted_data)
-        return join_result
+        sorted_data = sorted(dat)
+        result = "\n".join(sorted_data)
+        return result
+    else:
+        occurrence_dict = {}
+        sorted_data = []
+        for item in dat:
+            number_of_times = dat.count(item)
+            percentage = int(number_of_times / total * 100)
+            occurrence_dict[item] = [number_of_times, percentage]
+            sorted_data = sorted(occurrence_dict.items(), key=lambda item: (item[1][0], item[0]))
+
+        tmp_res = [f"{item[0]}: {item[1][0]} time(s), {item[1][1]}%" for item in sorted_data]
+        result = "\n".join(tmp_res)
+        return result
 
 
 def generate_result():
@@ -64,8 +73,12 @@ def generate_result():
 
     sorted_data = sort_data(data, sorting_type)
 
-    result_string = f"""Total {topic}: {total}.
+    if sorting_type == "natural":
+        result_string = f"""Total {topic}: {total}
 Sorted data: {is_line}{sorted_data}"""
+    else:
+        result_string = f"""Total {topic}: {total}.
+{sorted_data}"""
 
     return result_string
 
